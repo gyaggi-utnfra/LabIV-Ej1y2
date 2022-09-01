ea964f260cbbaf7921b0c22be2fa391ea4d5b4ed
gyaggi-github <gustavo_yaggi@hotmail.com>
2022-09-01 19:34:26 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  title = 'Promedio de edades';
+
+  edades: number[] = [];
+  promedio: number | undefined;
+
+  calcularPromedio(){
+    this.promedio = this.edades.reduce((avg,edad,_,{length}) => {
+      return avg + edad / length;
+    },0);
+  }
+
+  resetear()
+  {
+    this.edades.length = 0;
+    this.promedio = undefined;
+
+  }
+
+  mostrarPromedio()
+  {
+    if(
+      this.edades.length >= 2 && this.edades.every(x => x > 0)){
+        this.calcularPromedio();
+      }else{
+        alert("Edades invalidas. Ingrese dos enteros positivos");
+      }
+    
+  }
+}
